49132bf070f252304d161159e99ae42c850207bf
Nkematu Bonaventure <nkematu5@gmail.com>
2022-03-30 08:20:34 +0200

    updated verification middleware

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,39 +1,41 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
   const token = authHeader.split(" ")[1];
   if (authHeader) {
     jwt.verify(token, process.env.JWT_SEC, (err, user) => {
-      if (err){
-        res.status(403).json('Token is invalid');
-    }
+      if (err) {
+        res.status(403).json("Token is invalid");
+      }
       req.user = user;
-      next()
-    })
-} else {
-  res.status(401).json("you are not authenticated")
-}
-}
+      next();
+    });
+  } else {
+    res.status(401).json("you are not authenticated");
+  }
+};
 
 const verifyTokenAndAuth = (req, res, next) => {
-verifyToken(req, res, () => {
-if (req.user.id === req.params.id || req.user.isAdmin) {
-next()
-} else {
-res.status(403).json("You are not allowed to do that")
-}
-})
-}
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not allowed to do that");
+    }
+  });
+};
 
 const verifyAdmin = (req, res, next) => {
-if (req.user.isAdmin) {
-next()
-}
-}
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    }
+  });
+};
 
 module.exports = {
-verifyAdmin,
-verifyToken,
-verifyTokenAndAuth
-}
+  verifyAdmin,
+  verifyToken,
+  verifyTokenAndAuth,
+};
