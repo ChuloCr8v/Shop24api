f1d6d0ccb4dbc1b78711db5e375e08cdd469b720
Nkematu Bonaventure <nkematu5@gmail.com>
2022-03-31 11:54:39 +0200

    updates

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-const authRoute = require("./routes/auth")
-const userRoute = require("./routes/user")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
 
-dotenv.config()
-app.use(express.json())
+dotenv.config();
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send('Hello World')
-})
+  res.send("connection successful");
+});
 
-app.use("/auth", authRoute)
-app.use("/api/users", userRoute)
+app.use("/auth", authRoute);
+app.use("/api/users", userRoute);
 //app.use("/api", cloth)
 
-mongoose.connect(process.env.MONGO_URL).then(() => {
-  console.log('connection to DB successful')
-}).catch((error) => {
-  console.log(error)
-})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("connection to DB successful");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.listen(3000, () => {
-  console.log('server is listening on port 3000')
-})
+  console.log("server is listening on port 3000");
+});
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const {
   verifyToken,
   verifyTokenAndAuth,
-  verifyAdmin
+  verifyAdmin,
 } = require("./verifyToken");
 const router = require("express").Router();
 const User = require("../models/User");
@@ -33,41 +33,77 @@ router.put("/:id", verifyTokenAndAuth, async (req, res) => {
 });
 
 //Delete User
-router.delete('/:id', verifyTokenAndAuth, async (req, res) => {
+router.delete("/:id", verifyTokenAndAuth, async (req, res) => {
   try {
-    await User.findByIdAndDelete(req.params.id)
-    res.status(200).json('user has been deleted successfully')
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("user has been deleted successfully");
   } catch (e) {
-    res.status(400).json(e)
+    res.status(400).json(e);
   }
-})
+});
 
 //Get User
-router.get('/find/:id', verifyAdmin, async (req, res) => {
+router.get("/find/:id", verifyAdmin, async (req, res) => {
   try {
-    const user = await User.findById(req.params.id)
-    const {
-      password,
-      ...other
-    } = user._doc
-    res.status(200).json(user)
+    const user = await User.findById(req.params.id);
+    const { password, ...other } = user._doc;
+    res.status(200).json(user);
   } catch (e) {
-    res.status(400).json(e) 
+    res.status(400).json(e);
   }
-})
+});
 
 //Get all users
-router.get('/find', verifyAdmin, async (req, res) => {
-  const query = req.query.new
+router.get("/find", verifyAdmin, async (req, res) => {
+  const query = req.query.new;
   try {
-    const users = query ? await User.find().sort({
-      _id: -1
-    }).limit(1) : await User.find()
-    res.status(200).send(users)
+    const users = query
+      ? await User.find()
+          .sort({
+            _id: -1,
+          })
+          .limit(1)
+      : await User.find();
+    res.status(200).send(users);
   } catch (e) {
-    console.log(e)
-    res.status(500).json(e)
+    console.log(e);
+    res.status(500).json(e);
   }
-})
+});
+
+//Get User Stats
+
+router.get("/stats", verifyAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      {
+        $match: {
+          createdAt: {
+            $gte: lastYear,
+          },
+        },
+      },
+      {
+        $project: {
+          month: {
+            $month: "$createdAt",
+          },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
