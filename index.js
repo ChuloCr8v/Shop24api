658606758f9f039c77771d14bae08b4a4e686d26
Nkematu Bonaventure <nkematu5@gmail.com>
2022-04-04 23:25:53 +0200

    u

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const userRoute = require("./routes/user");
 const productRoute = require("./routes/product");
 const orderRoute = require("./routes/order");
 const cartRoute = require("./routes/cart");
-const stripeRoute = require("./routes/cart");
-const cors = require("cors")
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
+
 dotenv.config();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("connection successful");
 });
@@ -21,17 +22,17 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/cart", cartRoute);
-app.use("/api/checkout", cartRoute);
+app.use("/api/checkout", stripeRoute);
 //app.use("/api", cloth)
 
 mongoose
-.connect(process.env.MONGO_URL)
-.then(() => {
-  console.log("connection to DB successful");
-})
-.catch((error) => {
-  console.log(error);
-});
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("connection to DB successful");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.listen(5000, () => {
   console.log("server is listening on port 5000");
--- routes/stripe.js
+++ routes/stripe.js
@@ -1,16 +1,23 @@
-const router = require("express").Router()
-const stripe = require("stripe").process.env.STRIPE_SEC_KEY
+const router = require("express").Router();
+const stripe = require("stripe")(process.env.STRIPE_SEC_KEY);
 
 router.post("/payment", (req, res) => {
-  stripe.charges.create({
-    "source": req.body.tokenId,
-    "amount": req.body.amount,
-    "currency": "usd",
-  }, (stripeErr, stripeRes) => {
-    if(stripeErr){
-      res.status(500).json(stripeErr)
-    } else {
-      res.status(200).json(stripeRes)
+  stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "usd",
+      apiKey:
+        "sk_test_51KkiDeAUFaW3s2nQVKAGiCc10ItHHp4u1lBCXizdaLAFlGOfBTnPGoYq00OHu4XK6MwoUMAAUaVDEuQUmh6dylDs00Frc6chp9",
+    },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json(stripeErr);
+      } else {
+        res.status(200).json(stripeRes);
+      }
     }
-  })
-})
+  );
+});
+
+module.exports = router;
